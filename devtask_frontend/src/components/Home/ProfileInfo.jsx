7f8e499b44043e87974bbfe26e9c5243981dea28
Amarjit-paswan <amarjitp594@gmail.com>
2025-10-09 21:42:36 +0530

    add only few line of homePage

---
+++ devtask_frontend/src/components/Home/ProfileInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function ProfileInfo() {
+  return (
+    <>
+        <div className="container">
+            <div className="profile_container">
+                <div className="profile_img">
+                    <img src="" alt="" srcset="" />
+                </div>
+
+                <div className="profile_info">
+                    <h1>Hey, I'm Amarjit</h1>
+                    <p className="m-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum veniam consectetur ducimus magnam? Ipsa, nemo obcaecati exercitationem ea necessitatibus minima.</p>
+                </div>
+            </div>
+        </div>
+    
+    </>
+  )
+}
+
+export default ProfileInfo
---
+++ devtask_frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <div className="navbar">
+            <div className="logo">
+                <h2>DevTask</h2>
+            </div>
+
+            <div className="navitems_container">
+                <ul>
+                    <li>Home</li>
+                    <li>Profile</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    
+    </>
+  )
+}
+
+export default Navbar
