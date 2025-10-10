f09a5280d642c7194cb95bbbf08624988b31d634
Amarjit-paswan <amarjitp594@gmail.com>
2025-10-10 23:51:31 +0530

    add homepage design

--- devtask_frontend/src/components/Home/ProfileInfo.jsx
+++ devtask_frontend/src/components/Home/ProfileInfo.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import './home.css';
+import Card from './card';
 
 function ProfileInfo() {
   return (
     <>
-        <div className="container">
-            <div className="profile_container">
-                <div className="profile_img">
-                    <img src="" alt="" srcset="" />
+        <div className="container mt-5">
+            <div className="profile_container d-flex justify-content-around ">
+                <div className="profile_img  ">
+                    <img src="https://images.pexels.com/photos/1680175/pexels-photo-1680175.jpeg" alt="" srcset="" className='w-100 h-100' />
                 </div>
 
-                <div className="profile_info">
-                    <h1>Hey, I'm Amarjit</h1>
-                    <p className="m-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum veniam consectetur ducimus magnam? Ipsa, nemo obcaecati exercitationem ea necessitatibus minima.</p>
+                <div className="profile_info w-50  d-flex flex-column justify-content-center">
+                    <h1 className='text-warning'>Hey, I'm Amarjit</h1>
+                    <p className="m-1 fs-3">Full Stack Developer passionate about building interactive web-apps.</p>
+                    <div className="btn_groups d-flex gap-3 mt-4">
+                        <button className="btn btn-light btn-lg p-3 px-5">View My Projects</button>
+                        <button className="btn btn-none btn-lg p-3 px-5 border border-white text-white">Contact Me</button>
+                    </div>
                 </div>
+            </div>
+            <div className='card_container mt-5 d-flex justify-content-around  py-4 px-5'>
+
+                <Card />
+                <Card />
+                <Card />
             </div>
         </div>
     
--- devtask_frontend/src/components/Navbar/Navbar.jsx
+++ devtask_frontend/src/components/Navbar/Navbar.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <>
-        <div className="navbar">
-            <div className="logo">
+    <> 
+        <div className="container">
+        <div className="navbar  d-flex justify-content-between  ">
+            <div className="logo  px-2">
                 <h2>DevTask</h2>
             </div>
 
-            <div className="navitems_container">
-                <ul>
-                    <li>Home</li>
-                    <li>Profile</li>
-                    <li>Contact</li>
+            <div className="navitems_container w-75  d-flex justify-content-end">
+                <ul className='d-flex m-0 nav   justify-content-around w-75 align-items-center '>
+                    <li className='m-0 p-2 px-4 '>Home</li>
+                    <li className='m-0 p-2 px-4'>Profile</li>
+                    <li className='m-0 p-2 px-4'>Projects</li>
+                    <li className='m-0 p-2 px-4'>Tracker</li>
+                    <li className='m-0 p-2 px-4'>Contact</li>
                 </ul>
             </div>
+        </div>
         </div>
     
     </>
