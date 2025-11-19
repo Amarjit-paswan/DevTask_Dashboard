4750afbd11cd9ceb7e2001a35ad33faf71fc9067
Amarjit-paswan <amarjitp594@gmail.com>
2025-11-19 21:58:58 +0530

    add each project task modal box

--- devtask_frontend/src/components/Project/ProjectTaskModal.jsx
+++ devtask_frontend/src/components/Project/ProjectTaskModal.jsx
@@ -3,21 +3,52 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 function ProjectTaskModal() {
-  const project_id = useParams();
+  const {id} = useParams();
   const [task, setTask] = useState('');
-  console.log(project_id);
+  const [successMsg, setsuccessMsg] = useState('');
 
  
 
     const addTaskForm = async (e)=>{
       e.preventDefault();
       const data = {
-        'project_id' : project_id,
+        'project_id' : id,
         'task_name' : task
       };
       const res = await axios.post('http://localhost:8000/api/addTask',data);
       console.log(res);
+
+      //Show success message
+      if(res.data.status === 'success'){
+        setsuccessMsg(res.data.message);
+        setTask('');
+      }
+      
     }
+
+    const resetModalContent = ()=>{
+      setTask('');
+      setsuccessMsg('');
+    }
+
+    //Remove modal content when it's disappear
+    useEffect(()=>{
+      let modalElement = document.querySelector('#TaskAddModal');
+
+      const handleModalContent = ()=>{
+        resetModalContent();
+      }
+
+      if(modalElement){
+        modalElement.addEventListener('hidden.bs.modal', handleModalContent);
+      }
+
+        return ()=>{
+            if(modalElement){
+                modalElement.removeEventListener('hidden.bs.modal', handleModalContent);
+            }
+        }
+    },[])
   
 
 
@@ -31,7 +62,11 @@ function ProjectTaskModal() {
                 </div>
 
                   <form action="" method="post" onSubmit={addTaskForm}>
+                   
                     <div className="modal-body">
+                       { successMsg && (
+                          <div className="alert alert-success">{successMsg}</div>
+                        ) }
                       <div className="mb-3">
                         <label htmlFor="" className="form-label fw-bold">Task Name</label>
                         <input type="text" name="" id="" className="form-control" placeholder='Enter Your Task' onChange={(e)=> setTask(e.target.value)} value={task} />
